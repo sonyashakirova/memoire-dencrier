9b2cfd36f0e71b4003b1a5d5416cea19dfe94113
Sonya Shakirova <smshakirova@mail.ru>
2024-04-28 03:22:35 +0300

    feat: add fonts & tokens

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import './index.css';
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
