50c642cc2b03240ef2007f62ac9b3e45e7bd7c83
Sonya Shakirova <smshakirova@mail.ru>
2024-04-29 17:15:46 +0300

    feat: install lenis

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
+import Lenis from 'lenis';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import './styles/main.css';
+
+const lenis = new Lenis();
+
+const raf = (time: DOMHighResTimeStamp) => {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+};
+
+requestAnimationFrame(raf);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
